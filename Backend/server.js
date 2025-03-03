6b6370b057b5e1f60db73d76492984e7f025f150
m1xter <juan.vizcayag@gmail.com>
2025-03-03 19:17:53 -0400

    4- deployment configuration

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,7 @@ import postRoutes from  "./routes/postRoutes.js"
 import notificationRoutes from "./routes/notificationRoutes.js"
 import connectMongoDB from "./db/connectMongodb.js";
 import cookieParser from "cookie-parser";
-
+import path from "path"
 
 dotenv.config();
 
@@ -19,7 +19,7 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+const __dirname = path.resolve()
 app.use(express.json({limit:"5mb"}));
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
@@ -29,6 +29,14 @@ app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
 app.use("/api/posts",postRoutes);
 app.use("/api/notification",notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/Frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port: ${PORT}`);
